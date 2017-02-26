05cdcf401e8bf655e8705c7d9b9ad5369bf0eea0
Gilad Reich <giladreichgr@gmail.com>
2017-02-26 21:03:17 +0100

    * Updated database communication API with the new tables.

--- BinnenFA54Project/Frameworks/Database/Db.designer.cs
+++ BinnenFA54Project/Frameworks/Database/Db.designer.cs
@@ -33,6 +33,9 @@ namespace BinnenFA54Project.Frameworks.Database
     partial void InsertT_SBF_Binnen(T_SBF_Binnen instance);
     partial void UpdateT_SBF_Binnen(T_SBF_Binnen instance);
     partial void DeleteT_SBF_Binnen(T_SBF_Binnen instance);
+    partial void InsertT_Fragenbogen_Thema(T_Fragenbogen_Thema instance);
+    partial void UpdateT_Fragenbogen_Thema(T_Fragenbogen_Thema instance);
+    partial void DeleteT_Fragenbogen_Thema(T_Fragenbogen_Thema instance);
     #endregion
 		
 		public DbDataContext() : 
@@ -78,6 +81,22 @@ namespace BinnenFA54Project.Frameworks.Database
 			get
 			{
 				return this.GetTable<T_SBF_Binnen>();
+			}
+		}
+		
+		public System.Data.Linq.Table<T_Fragenbogen_Thema> T_Fragenbogen_Themas
+		{
+			get
+			{
+				return this.GetTable<T_Fragenbogen_Thema>();
+			}
+		}
+		
+		public System.Data.Linq.Table<View_Topics_And_Question> View_Topics_And_Questions
+		{
+			get
+			{
+				return this.GetTable<View_Topics_And_Question>();
 			}
 		}
 	}
@@ -329,6 +348,269 @@ namespace BinnenFA54Project.Frameworks.Database
 			if ((this.PropertyChanged != null))
 			{
 				this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+	}
+	
+	[global::System.Data.Linq.Mapping.TableAttribute(Name="dbo.T_Fragenbogen_Thema")]
+	public partial class T_Fragenbogen_Thema : INotifyPropertyChanging, INotifyPropertyChanged
+	{
+		
+		private static PropertyChangingEventArgs emptyChangingEventArgs = new PropertyChangingEventArgs(String.Empty);
+		
+		private int _FragebogenNr;
+		
+		private string _Thema;
+		
+		private string _Beschreib;
+		
+    #region Extensibility Method Definitions
+    partial void OnLoaded();
+    partial void OnValidate(System.Data.Linq.ChangeAction action);
+    partial void OnCreated();
+    partial void OnFragebogenNrChanging(int value);
+    partial void OnFragebogenNrChanged();
+    partial void OnThemaChanging(string value);
+    partial void OnThemaChanged();
+    partial void OnBeschreibChanging(string value);
+    partial void OnBeschreibChanged();
+    #endregion
+		
+		public T_Fragenbogen_Thema()
+		{
+			OnCreated();
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_FragebogenNr", DbType="Int NOT NULL", IsPrimaryKey=true)]
+		public int FragebogenNr
+		{
+			get
+			{
+				return this._FragebogenNr;
+			}
+			set
+			{
+				if ((this._FragebogenNr != value))
+				{
+					this.OnFragebogenNrChanging(value);
+					this.SendPropertyChanging();
+					this._FragebogenNr = value;
+					this.SendPropertyChanged("FragebogenNr");
+					this.OnFragebogenNrChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Thema", DbType="NChar(20) NOT NULL", CanBeNull=false)]
+		public string Thema
+		{
+			get
+			{
+				return this._Thema;
+			}
+			set
+			{
+				if ((this._Thema != value))
+				{
+					this.OnThemaChanging(value);
+					this.SendPropertyChanging();
+					this._Thema = value;
+					this.SendPropertyChanged("Thema");
+					this.OnThemaChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Beschreib", DbType="NChar(100)")]
+		public string Beschreib
+		{
+			get
+			{
+				return this._Beschreib;
+			}
+			set
+			{
+				if ((this._Beschreib != value))
+				{
+					this.OnBeschreibChanging(value);
+					this.SendPropertyChanging();
+					this._Beschreib = value;
+					this.SendPropertyChanged("Beschreib");
+					this.OnBeschreibChanged();
+				}
+			}
+		}
+		
+		public event PropertyChangingEventHandler PropertyChanging;
+		
+		public event PropertyChangedEventHandler PropertyChanged;
+		
+		protected virtual void SendPropertyChanging()
+		{
+			if ((this.PropertyChanging != null))
+			{
+				this.PropertyChanging(this, emptyChangingEventArgs);
+			}
+		}
+		
+		protected virtual void SendPropertyChanged(String propertyName)
+		{
+			if ((this.PropertyChanged != null))
+			{
+				this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+	}
+	
+	[global::System.Data.Linq.Mapping.TableAttribute(Name="dbo.View_Topics_And_Questions")]
+	public partial class View_Topics_And_Question
+	{
+		
+		private int _P_Id;
+		
+		private string _Frage;
+		
+		private string _Antwort1;
+		
+		private string _Antwort2;
+		
+		private string _Antwort3;
+		
+		private string _Antwort4;
+		
+		private int _FragebogenNr;
+		
+		private System.Nullable<byte> _RichtigeAntwort;
+		
+		public View_Topics_And_Question()
+		{
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_P_Id", DbType="Int NOT NULL")]
+		public int P_Id
+		{
+			get
+			{
+				return this._P_Id;
+			}
+			set
+			{
+				if ((this._P_Id != value))
+				{
+					this._P_Id = value;
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Frage", DbType="VarChar(500)")]
+		public string Frage
+		{
+			get
+			{
+				return this._Frage;
+			}
+			set
+			{
+				if ((this._Frage != value))
+				{
+					this._Frage = value;
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Antwort1", DbType="VarChar(500)")]
+		public string Antwort1
+		{
+			get
+			{
+				return this._Antwort1;
+			}
+			set
+			{
+				if ((this._Antwort1 != value))
+				{
+					this._Antwort1 = value;
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Antwort2", DbType="VarChar(500)")]
+		public string Antwort2
+		{
+			get
+			{
+				return this._Antwort2;
+			}
+			set
+			{
+				if ((this._Antwort2 != value))
+				{
+					this._Antwort2 = value;
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Antwort3", DbType="VarChar(500)")]
+		public string Antwort3
+		{
+			get
+			{
+				return this._Antwort3;
+			}
+			set
+			{
+				if ((this._Antwort3 != value))
+				{
+					this._Antwort3 = value;
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Antwort4", DbType="VarChar(500)")]
+		public string Antwort4
+		{
+			get
+			{
+				return this._Antwort4;
+			}
+			set
+			{
+				if ((this._Antwort4 != value))
+				{
+					this._Antwort4 = value;
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_FragebogenNr", DbType="Int NOT NULL")]
+		public int FragebogenNr
+		{
+			get
+			{
+				return this._FragebogenNr;
+			}
+			set
+			{
+				if ((this._FragebogenNr != value))
+				{
+					this._FragebogenNr = value;
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_RichtigeAntwort", DbType="TinyInt")]
+		public System.Nullable<byte> RichtigeAntwort
+		{
+			get
+			{
+				return this._RichtigeAntwort;
+			}
+			set
+			{
+				if ((this._RichtigeAntwort != value))
+				{
+					this._RichtigeAntwort = value;
+				}
 			}
 		}
 	}
