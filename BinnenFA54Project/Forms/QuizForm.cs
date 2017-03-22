e3ada5560ccfa39cc68acd51a7303594fe884832
Gilad Reich <giladreichgr@gmail.com>
2017-03-22 01:04:44 +0100

    * Updating the behavior of the QuizForm, question number display right to the corresponding question and we accessing the questions and answers statically.

--- BinnenFA54Project/Forms/QuizForm.cs
+++ BinnenFA54Project/Forms/QuizForm.cs
@@ -8,21 +8,26 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BinnenFA54Project.Main;
 using BinnenFA54Project.Main.ServeData;
 
 namespace BinnenFA54Project.Forms
 {
     public partial class QuizForm : Form
     {
-		// TODO: Create a progress bar to update when questions index changing.
-        private QuestionMgr _questionMgr;
-        private int questionIndexer = 0;
+        // TODO: Create a progress bar to update when questions index changing.
+        QuestionMgr questions;
+        AnswerMgr answers;
+        FeedbackForm _feedbackForm;
+        int qIndex = 0; // Question Indexer.
 
         public QuizForm()
         {
             InitializeComponent();
+            // Creating manageable locally objects.
+            questions = QuizMgr.Questions;
+            answers   = QuizMgr.Answers;
 
-            _questionMgr = new QuestionMgr();
             //Thread.Sleep(1000); // TODO: create during that time loading indicator runs on another thread.
             UpdateQuestions();
         }
@@ -30,39 +35,36 @@ namespace BinnenFA54Project.Forms
 
         private void UpdateQuestions()
         {
-            lblQuestionIndexer.Text = (questionIndexer + 1).ToString(); // +1 because array starts from 0 while db from 1.
-            lblQuestion.Text        = _questionMgr.QuestionList[questionIndexer].QuestionMeoww;
-            cbCombo.CheckBox1Text   = _questionMgr.QuestionList[questionIndexer].Options[0];
-            cbCombo.CheckBox2Text   = _questionMgr.QuestionList[questionIndexer].Options[1];
-            cbCombo.CheckBox3Text   = _questionMgr.QuestionList[questionIndexer].Options[2];
-            cbCombo.CheckBox4Text   = _questionMgr.QuestionList[questionIndexer].Options[3];
+            lblQuestion.Text        = questions.QuestionList[qIndex].QuestionMeoww;
+            cbCombo.CheckBox1Text   = questions.QuestionList[qIndex].Options[0];
+            cbCombo.CheckBox2Text   = questions.QuestionList[qIndex].Options[1];
+            cbCombo.CheckBox3Text   = questions.QuestionList[qIndex].Options[2];
+            cbCombo.CheckBox4Text   = questions.QuestionList[qIndex].Options[3];
+            // db based 1 value while _questions.QuestionList<List> based 0.
+            lblQuestionIndexer.Text = (qIndex + 1).ToString();
         }
 
         private void btnNext_Click(object sender, EventArgs e)
         {
-            questionIndexer++;
+            qIndex++;
 
-
-            if (questionIndexer > 0)
+            if (qIndex == 1)
                 btnBack.ButtonEnabled = true;
 
-            if (questionIndexer == _questionMgr.QuestionList.Count - 1) // last question.
-            {
+            if (qIndex == questions.QuestionList.Count - 1) // last question.
                 btnNext.ButtonEnabled = false;
-                return;
-            }
 
             UpdateQuestions();
         }
 
         private void btnBack_Click(object sender, EventArgs e)
         {
-            questionIndexer--;
+            qIndex--;
 
-            if (questionIndexer == 0)
+            if (qIndex == 0)
                 btnBack.ButtonEnabled = false;
 
-            if (questionIndexer == _questionMgr.QuestionList.Count - 2)
+            if (qIndex == questions.QuestionList.Count - 2)
                 btnNext.ButtonEnabled = true;
 
             UpdateQuestions();
