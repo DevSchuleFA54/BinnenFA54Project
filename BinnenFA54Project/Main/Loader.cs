f4373b8edd7baadd68d40a8bda438a7047b4f6e1
Togi <Tolgahan.a@hotmail.de>
2017-03-30 18:22:29 +0200

    * Created a special class to start the loader on different threads, so it doesn't freeze the application.

---
+++ BinnenFA54Project/Main/Loader.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BinnenFA54Project.Forms.Misc;
+
+namespace BinnenFA54Project.Main
+{
+    public enum LoaderSelector
+    {
+        Loader        = 0,
+        MainAppLoader = 1
+    }
+
+    class Loader
+    {
+        private static Thread _loader;
+        private static LoaderSelector _loaderSelector;
+
+
+
+        public static void StartLoader(LoaderSelector loader)
+        {
+            _loaderSelector = loader;
+            _loader = new Thread(new ThreadStart(Load));
+            _loader.Start();
+        }
+
+
+
+        public static void StopLoader()
+        {
+            try
+            {
+                _loader.Abort();
+            }
+            catch (ThreadAbortException)
+            {
+                Thread.ResetAbort();
+            }
+
+        }
+
+
+        private static void Load()
+        {
+            if (LoaderSelector.MainAppLoader == _loaderSelector)
+            {
+                MainAppLoader appMainLoader = new MainAppLoader();
+                Application.Run(appMainLoader);
+            }
+            else
+            {
+                LoaderForm loader = new LoaderForm();
+                Application.Run(loader);
+            }
+        }
+
+
+    }
+}
