913d8321b2b1b208ac388f58dcd8c9ef667a7056
Tolga Aydin <tolgahan.a@hotmail.de>
2017-04-25 20:06:18 +0200

    Quizform german translation

--- BinnenFA54Project/Forms/QuizForm.Designer.cs
+++ BinnenFA54Project/Forms/QuizForm.Designer.cs
@@ -50,118 +50,70 @@
             // 
             // progressBar
             // 
-            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.progressBar.Location = new System.Drawing.Point(157, 407);
+            resources.ApplyResources(this.progressBar, "progressBar");
             this.progressBar.Name = "progressBar";
-            this.progressBar.Size = new System.Drawing.Size(515, 31);
-            this.progressBar.TabIndex = 1;
             // 
             // lblExamNum
             // 
-            this.lblExamNum.AutoSize = true;
+            resources.ApplyResources(this.lblExamNum, "lblExamNum");
             this.lblExamNum.BackColor = System.Drawing.Color.Transparent;
-            this.lblExamNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblExamNum.ForeColor = System.Drawing.Color.Azure;
-            this.lblExamNum.Location = new System.Drawing.Point(8, 17);
             this.lblExamNum.Name = "lblExamNum";
-            this.lblExamNum.Size = new System.Drawing.Size(107, 17);
-            this.lblExamNum.TabIndex = 2;
-            this.lblExamNum.Text = "Exam Number";
             // 
             // lblQuestion
             // 
+            resources.ApplyResources(this.lblQuestion, "lblQuestion");
             this.lblQuestion.BackColor = System.Drawing.Color.Transparent;
-            this.lblQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblQuestion.ForeColor = System.Drawing.Color.Azure;
-            this.lblQuestion.Location = new System.Drawing.Point(48, 61);
             this.lblQuestion.Name = "lblQuestion";
-            this.lblQuestion.Size = new System.Drawing.Size(655, 32);
-            this.lblQuestion.TabIndex = 2;
-            this.lblQuestion.Text = "Question Text";
             // 
             // label4
             // 
-            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.label4.AutoSize = true;
+            resources.ApplyResources(this.label4, "label4");
             this.label4.BackColor = System.Drawing.Color.Transparent;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Azure;
-            this.label4.Location = new System.Drawing.Point(91, 417);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(60, 15);
-            this.label4.TabIndex = 2;
-            this.label4.Text = "Go Back";
             // 
             // label5
             // 
-            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.label5.AutoSize = true;
+            resources.ApplyResources(this.label5, "label5");
             this.label5.BackColor = System.Drawing.Color.Transparent;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.Azure;
-            this.label5.Location = new System.Drawing.Point(678, 417);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(97, 15);
-            this.label5.TabIndex = 2;
-            this.label5.Text = "Next Question";
             // 
             // btnFinish
             // 
-            this.btnFinish.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            resources.ApplyResources(this.btnFinish, "btnFinish");
             this.btnFinish.BackColor = System.Drawing.SystemColors.HotTrack;
-            this.btnFinish.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnFinish.ForeColor = System.Drawing.Color.Azure;
-            this.btnFinish.Location = new System.Drawing.Point(753, 3);
             this.btnFinish.Name = "btnFinish";
-            this.btnFinish.Size = new System.Drawing.Size(92, 40);
-            this.btnFinish.TabIndex = 5;
-            this.btnFinish.Text = "Finish Exam";
             this.btnFinish.UseVisualStyleBackColor = false;
             this.btnFinish.Click += new System.EventHandler(this.btnFinish_Click);
             // 
             // lblQuestionIndexer
             // 
-            this.lblQuestionIndexer.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.lblQuestionIndexer.AutoSize = true;
+            resources.ApplyResources(this.lblQuestionIndexer, "lblQuestionIndexer");
             this.lblQuestionIndexer.BackColor = System.Drawing.Color.Transparent;
-            this.lblQuestionIndexer.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblQuestionIndexer.ForeColor = System.Drawing.Color.Cornsilk;
-            this.lblQuestionIndexer.Location = new System.Drawing.Point(456, 447);
             this.lblQuestionIndexer.Name = "lblQuestionIndexer";
-            this.lblQuestionIndexer.Size = new System.Drawing.Size(14, 13);
-            this.lblQuestionIndexer.TabIndex = 2;
-            this.lblQuestionIndexer.Text = "0";
             // 
             // label1
             // 
-            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.label1.AutoSize = true;
+            resources.ApplyResources(this.label1, "label1");
             this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Azure;
-            this.label1.Location = new System.Drawing.Point(355, 447);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(112, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Question Number: ";
             // 
             // pbSign
             // 
-            this.pbSign.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            resources.ApplyResources(this.pbSign, "pbSign");
             this.pbSign.BackColor = System.Drawing.Color.Transparent;
-            this.pbSign.Location = new System.Drawing.Point(681, 145);
             this.pbSign.Name = "pbSign";
-            this.pbSign.Size = new System.Drawing.Size(164, 167);
-            this.pbSign.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pbSign.TabIndex = 9;
             this.pbSign.TabStop = false;
             // 
             // cbCombo
             // 
-            this.cbCombo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            resources.ApplyResources(this.cbCombo, "cbCombo");
             this.cbCombo.BackColor = System.Drawing.Color.Transparent;
             this.cbCombo.CheckBox1Text = "CheckBox Text";
             this.cbCombo.CheckBox2Text = "CheckBox Text";
@@ -169,48 +121,39 @@
             this.cbCombo.CheckBox4Text = "CheckBox Text";
             this.cbCombo.CheckBoxAutoSize = false;
             this.cbCombo.CheckBoxFont = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cbCombo.CheckBoxLabelSize = new System.Drawing.Size(546, 60);
-            this.cbCombo.CheckBoxSize = new System.Drawing.Size(587, 60);
+            this.cbCombo.CheckBoxLabelSize = new System.Drawing.Size(818, 86);
+            this.cbCombo.CheckBoxSize = new System.Drawing.Size(878, 86);
             this.cbCombo.HandCursorHover = false;
             this.cbCombo.LabelForeColor = System.Drawing.Color.Azure;
             this.cbCombo.LabelForeColorDisabled = System.Drawing.Color.Gray;
             this.cbCombo.LabelForeColorHover = System.Drawing.Color.White;
-            this.cbCombo.Location = new System.Drawing.Point(82, 96);
-            this.cbCombo.MinimumSize = new System.Drawing.Size(230, 150);
             this.cbCombo.Name = "cbCombo";
-            this.cbCombo.Size = new System.Drawing.Size(593, 267);
-            this.cbCombo.TabIndex = 8;
             this.cbCombo.ViewModeState = GiladControllers.ControlViewMode.Active;
             // 
             // btnBack
             // 
-            this.btnBack.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            resources.ApplyResources(this.btnBack, "btnBack");
             this.btnBack.AnimateHover = true;
             this.btnBack.ButtonDirection = GiladControllers.CustomButtonDirection.Back;
             this.btnBack.ButtonEnabled = false;
             this.btnBack.Cursor = System.Windows.Forms.Cursors.Default;
             this.btnBack.HandCursor = true;
-            this.btnBack.Location = new System.Drawing.Point(6, 385);
             this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(75, 75);
-            this.btnBack.TabIndex = 7;
             this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
             // btnNext
             // 
-            this.btnNext.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            resources.ApplyResources(this.btnNext, "btnNext");
             this.btnNext.AnimateHover = true;
             this.btnNext.ButtonDirection = GiladControllers.CustomButtonDirection.Forward;
             this.btnNext.ButtonEnabled = true;
             this.btnNext.HandCursor = true;
-            this.btnNext.Location = new System.Drawing.Point(781, 385);
             this.btnNext.Name = "btnNext";
-            this.btnNext.Size = new System.Drawing.Size(75, 75);
-            this.btnNext.TabIndex = 7;
             this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
             // 
             // giladGradientPanel1
             // 
+            resources.ApplyResources(this.giladGradientPanel1, "giladGradientPanel1");
             this.giladGradientPanel1.BackColor = System.Drawing.Color.Transparent;
             this.giladGradientPanel1.Color1 = System.Drawing.Color.Transparent;
             this.giladGradientPanel1.Color2 = System.Drawing.Color.DarkSlateBlue;
@@ -227,28 +170,22 @@
             this.giladGradientPanel1.Controls.Add(this.label4);
             this.giladGradientPanel1.Controls.Add(this.label5);
             this.giladGradientPanel1.CustomCursor = true;
-            this.giladGradientPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.giladGradientPanel1.DraggableForm = true;
             this.giladGradientPanel1.GradientMode = System.Drawing.Drawing2D.LinearGradientMode.Vertical;
-            this.giladGradientPanel1.Location = new System.Drawing.Point(9, 50);
             this.giladGradientPanel1.Name = "giladGradientPanel1";
-            this.giladGradientPanel1.Size = new System.Drawing.Size(855, 535);
-            this.giladGradientPanel1.TabIndex = 10;
             // 
             // giladControlBox1
             // 
-            this.giladControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            resources.ApplyResources(this.giladControlBox1, "giladControlBox1");
             this.giladControlBox1.BackColor = System.Drawing.Color.Transparent;
             this.giladControlBox1.Color1 = System.Drawing.Color.White;
             this.giladControlBox1.Color2 = System.Drawing.Color.Black;
             this.giladControlBox1.GradientMode = System.Drawing.Drawing2D.LinearGradientMode.Vertical;
-            this.giladControlBox1.Location = new System.Drawing.Point(747, 9);
             this.giladControlBox1.Name = "giladControlBox1";
-            this.giladControlBox1.Size = new System.Drawing.Size(115, 40);
-            this.giladControlBox1.TabIndex = 11;
             // 
             // QuizForm
             // 
+            resources.ApplyResources(this, "$this");
             this.AppTitle.Icon = ((System.Drawing.Icon)(resources.GetObject("resource.Icon")));
             this.AppTitle.IconLocation = new System.Drawing.Point(10, 10);
             this.AppTitle.IconSize = new System.Drawing.Size(32, 32);
@@ -257,10 +194,8 @@
             this.AppTitle.TextColor = System.Drawing.Color.Bisque;
             this.AppTitle.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.AppTitle.TextLocation = new System.Drawing.Point(50, 15);
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.WindowFrame;
-            this.ClientSize = new System.Drawing.Size(872, 594);
             this.Controls.Add(this.giladControlBox1);
             this.Controls.Add(this.giladGradientPanel1);
             this.CustomCursor = true;
@@ -271,11 +206,8 @@
             this.FormBorders.DrawBorders = true;
             this.FormBorders.Width = 8;
             this.KeyPreview = true;
-            this.MinimumSize = new System.Drawing.Size(850, 560);
             this.Name = "QuizForm";
             this.ResizeGripColor = System.Drawing.Color.Lavender;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Application Title";
             this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.QuizForm_KeyPress);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.QuizForm_KeyUp);
             ((System.ComponentModel.ISupportInitialize)(this.pbSign)).EndInit();
